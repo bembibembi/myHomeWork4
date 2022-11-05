b70c89b366f231b9f6b853223633f9daa0af4140
bembibembi <116353834+bembibembi@users.noreply.github.com>
2022-11-05 17:23:57 +0600

    Update main.swift

--- myHomeWork4/main.swift
+++ myHomeWork4/main.swift
@@ -1,11 +1,53 @@
-//
-//  main.swift
-//  myHomeWork4
-//
-//  Created by 지구9 on 5/11/22.
-//
+var twos: String = ""
+var threes: String = ""
+var fours = ""
+var fives = ""
 
-import Foundation
+for number in 1...1000{
+    
+    if number % 2 == 0 {
+        twos += "\(number), "
+    }else if number % 3 == 0 {
+        threes += "\(number),"
+    }else if number % 4 == 0{
+        fours += "\(number),"
+    }else if number % 5 == 0{
+        fives += "\(number),"
+    }
+}
+ print("числа , кратные 2) : \(twos),")
+print("числа , кратные 3) : \(threes),")
+print("числа , кратные 4) : \(fours),")
+print( "числа , кратные 5) : \(fives),")
 
-print("Hello, World!")
+
+//var words = "Яблоко,Морковь,Огурец,Баклажан,Дом,Машина,Слово,Кошка,Собака,Работа,Друзья,Ноутбук,Бутылка,Клавиатура,Книга,Сумка,Кактус,Растение,Очки,Картинка"
+var words =
+    "фотография,апарвт,создание,салат,окно,самолет,предлог,крыса,орган,номер,друг,планшет,сок,алмаз,вишня,лимон ,айфон,гаджет,курут,меломан "
+
+func showWordsData(word: String){
+    
+    var currentWord = ""
+    var lettersCounter = 0
+    
+    
+    for symbol in words{
+        
+        if symbol != ","{
+            currentWord += "\(symbol)"
+            lettersCounter += 1
+        } else {
+            print("Слово: \(currentWord). Количество символов -  \(lettersCounter).")
+            currentWord = ""
+            lettersCounter = 0
+        }
+    }
+    
+}
+
+showWordsData(word: words)
+
+
+
+
 
